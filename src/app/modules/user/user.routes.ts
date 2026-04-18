4aa7e0becbfa9ceb50a37271295c2a314e0229c9
unknown <cse12005038brur@gmail.com>
2026-04-18 23:51:15 +0600

    added pagination, filtering, searching for users

--- src/app/modules/user/user.routes.ts
+++ src/app/modules/user/user.routes.ts
@@ -2,8 +2,17 @@ import express, { NextFunction, Request, Response } from 'express'
 import { UserController } from './user.controller';
 import { fileUploader } from '../../helper/fileUploader';
 import { UserValidation } from './user.validation';
+import { UserRole } from '@prisma/client';
+import auth from '../../middlewares/auth';
+
 
 const router = express.Router();
+
+router.get(
+    "/",
+    auth(UserRole.ADMIN),
+    UserController.getAllFromDB
+)
 
 router.post(
     "/create-patient",
@@ -12,10 +21,27 @@ router.post(
         req.body = UserValidation.createPatientValidationSchema.parse(JSON.parse(req.body.data))
         return UserController.createPatient(req, res, next)
     }
-
 )
 
-// create doctor
-// create admin
+router.post(
+    "/create-admin",
+    auth(UserRole.ADMIN),
+    fileUploader.upload.single('file'),
+    (req: Request, res: Response, next: NextFunction) => {
+        req.body = UserValidation.createAdminValidationSchema.parse(JSON.parse(req.body.data))
+        return UserController.createAdmin(req, res, next)
+    }
+);
+
+router.post(
+    "/create-doctor",
+    auth(UserRole.ADMIN),
+    fileUploader.upload.single('file'),
+    (req: Request, res: Response, next: NextFunction) => {
+        console.log(JSON.parse(req.body.data))
+        req.body = UserValidation.createDoctorValidationSchema.parse(JSON.parse(req.body.data))
+        return UserController.createDoctor(req, res, next)
+    }
+);
 
 export const userRoutes = router;
